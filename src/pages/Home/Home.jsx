6d9b3bc67055bccf6f4864ef2294f686144249b0
Lee Chong Yu <leechongyu99@gmail.com>
2026-05-07 22:33:38 +0800

    fix ui

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,40 +6,32 @@ function Home() {
   const navigate = useNavigate();
 
   const products = [
-    { id: 1, name: "Iphone", price: "RM3999", img: "https://via.placeholder.com/150"},
-    { id: 2, name: "ipad", price: "RM39999", img: "https://via.placeholder.com/150"},
-    { id: 3, name: "macbook", price: "RM93999", img: "https://via.placeholder.com/150"},
+    { id: 1, name: "Iphone", price: "RM3999", img: "https://via.placeholder.com/150" },
+    { id: 2, name: "iPad", price: "RM39999", img: "https://via.placeholder.com/150" },
+    { id: 3, name: "MacBook", price: "RM93999", img: "https://via.placeholder.com/150" },
   ];
 
   return (
     <div className="page">
 
-      <div className="layout">
-        
-        <div className="content">
-          <h1>My Shop</h1>
+      <h1>My Shop</h1>
 
-            <div className="slidingcontent">
-            <Slider />
-            </div>
+      <Slider />
 
-          <div className="grid">
-            {products.map((p) => (
-              <div
-                className="card"
-                key={p.id}
-                onClick={() => navigate(`/product/${p.id}`)}
-              >
-                <img src={p.img} alt={p.name} />
-                <h3>{p.name}</h3>
-                <p>{p.price}</p>
-              </div>
-            ))}
+      <div className="grid">
+        {products.map((p) => (
+          <div
+            className="card"
+            key={p.id}
+            onClick={() => navigate(`/product/${p.id}`)}
+          >
+            <img src={p.img} alt={p.name} />
+            <h3>{p.name}</h3>
+            <p>{p.price}</p>
           </div>
-
-        </div>
-
+        ))}
       </div>
+
     </div>
   );
 }
